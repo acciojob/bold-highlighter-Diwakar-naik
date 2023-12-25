7a6009ebc5298fe95bdb060fe9eedf520060e6b0
Diwakar-naik <137063932+Diwakar-naik@users.noreply.github.com>
2023-12-25 10:39:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// script.js
+
 function highlight() {
-    //Write your code here
+  // Get all <strong> elements within the paragraphs
+  var boldElements = document.querySelectorAll('p strong');
 
-
+  // Change the color of bold elements to green
+  boldElements.forEach(function (element) {
+    element.style.color = 'green';
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
+  // Get all <strong> elements within the paragraphs
+  var boldElements = document.querySelectorAll('p strong');
 
-    
+  // Revert the color of bold elements back to black
+  boldElements.forEach(function (element) {
+    element.style.color = 'black';
+  });
 }
